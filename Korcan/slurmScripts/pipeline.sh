63417b475c65713ddcec9eea879c960d96cc7f63
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-01-06 01:25:58 -0800

    general

--- Korcan/slurmScripts/pipeline.sh
+++ Korcan/slurmScripts/pipeline.sh
@@ -4,8 +4,7 @@
 #SBATCH --mem=8G               # memory per node
 #SBATCH --time=00:05:00
 
-module load python/3.10
+module load python/3.8.10
 module load scipy-stack
-source ../pipelineStack/bin/activate &&
-#nvidia-smi
+source ../uneqENV/bin/activate &&
 python Korcan/pipeline.py
