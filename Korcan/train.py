e24869322086e14ceb0f59de11197cc68dc08f5b
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-01-18 04:49:27 -0800

    mcl

--- Korcan/train.py
+++ Korcan/train.py
@@ -9,23 +9,22 @@ from models.quantizer import QLayer
 
 np.seterr(over="raise")
 
-def generate_arrays_from_file(HMtrainDIR,trainDir):
-    while 1:
-        # xTrainData = []
-        # yTrainData = []
-        folderNamesTrain = [name for name in os.listdir(HMtrainDIR)]
-        for fo in folderNamesTrain:
-            TrainFileNames = [name for name in os.listdir(os.path.join(trainDir,fo)) if os.path.isfile(os.path.join(trainDir,fo,name))]
-            HMTrainFileNames = [name for name in os.listdir(os.path.join(HMtrainDIR,fo)) if os.path.isfile(os.path.join(HMtrainDIR,fo,name))]
-            for i in range(len(TrainFileNames)):
-                I = tf.keras.preprocessing.image.load_img(os.path.join(trainDir,fo,TrainFileNames[i]))
-                I = I.resize([224, 224])
-                im_array = tf.keras.preprocessing.image.img_to_array(I)
-                targetTensor = np.load(os.path.join(HMtrainDIR,fo,HMTrainFileNames[i]))
-                if im_array is None:
-                    print("im_array is None")
-                yield(np.expand_dims(im_array, axis=0),targetTensor)
-
+# def generate_arrays_from_file(HMtrainDIR,trainDir):
+#     while 1:
+#         # xTrainData = []
+#         # yTrainData = []
+#         folderNamesTrain = [name for name in os.listdir(HMtrainDIR)]
+#         for fo in folderNamesTrain:
+#             TrainFileNames = [name for name in os.listdir(os.path.join(trainDir,fo)) if os.path.isfile(os.path.join(trainDir,fo,name))]
+#             HMTrainFileNames = [name for name in os.listdir(os.path.join(HMtrainDIR,fo)) if os.path.isfile(os.path.join(HMtrainDIR,fo,name))]
+#             for i in range(len(TrainFileNames)):
+#                 I = tf.keras.preprocessing.image.load_img(os.path.join(trainDir,fo,TrainFileNames[i]))
+#                 I = I.resize([224, 224])
+#                 im_array = tf.keras.preprocessing.image.img_to_array(I)
+#                 targetTensor = np.load(os.path.join(HMtrainDIR,fo,HMTrainFileNames[i]))
+#                 if im_array is None:
+#                     print("im_array is None")
+#                 yield(np.expand_dims(im_array, axis=0),targetTensor)
 
 def loadModel(modelName, splitLayer):
     modelPath = "deep_models_full/" + modelName + "_model.h5"
@@ -68,20 +67,22 @@ if __name__ == "__main__":
     # splitLayer = "add_1"
     # modelName = "dense"
     # splitLayer = "pool2_conv"
-    valDir = "/home/foniks/scratch/datasets/ILSVRC2012_img_val"
-    trainDir = "/home/foniks/scratch/datasets/ILSVRC2012_img_trainNew"
+    valDir = "/localhome/kuyanik/datasets/ILSVRC2012_img_val"
+    trainDir = "/localhome/kuyanik/dataset/ILSVRC2012_img_trainNew"
+    # valDir = "/media/sf_Downloads/datasetILSVRC/ILSVRC2012_img_val"
+    # trainDir = "/media/sf_Downloads/datasetILSVRC/ILSVRC2012_img_trainNew"
     HMvalDIR = valDir+"_HM_"+modelName+"_"+splitLayer
     HMtrainDIR = trainDir+"_HM_"+modelName+"_"+splitLayer
 
-    gpus = tf.config.list_physical_devices('GPU')
-    tf.config.experimental.set_memory_growth(gpus[0], True)
+    # gpus = tf.config.list_physical_devices('GPU')
+    # tf.config.experimental.set_memory_growth(gpus[0], True)
 
     mobileModel = loadModel(modelName, splitLayer)
     mobileModel.trainable = True
     mobileModel.compile(optimizer=tf.keras.optimizers.Adam(1e-1),
                 loss=tf.keras.losses.MeanSquaredError(),)
 
-    reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1,patience=3, min_lr=1e-12)
+    reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1,patience=3, min_lr=1e-15)
     esCallback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
     checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath='checkpoints/model.{epoch:02d}-{val_loss:.2f}.h5')
     xValidationData = []
@@ -99,10 +100,29 @@ if __name__ == "__main__":
     xValidationData = np.array(xValidationData)
     yValidationData = np.array(yValidationData)
 
+    xTrainData = []
+    yTrainData = []
+    folderNamesTrain = [name for name in os.listdir(HMtrainDIR)]
+    for fo in folderNamesTrain:
+        TrainFileNames = [name for name in os.listdir(os.path.join(trainDir,fo)) if os.path.isfile(os.path.join(trainDir,fo,name))]
+        HMTrainFileNames = [name for name in os.listdir(os.path.join(HMtrainDIR,fo)) if os.path.isfile(os.path.join(HMtrainDIR,fo,name))]
+        for i in range(len(TrainFileNames)):
+            I = tf.keras.preprocessing.image.load_img(os.path.join(trainDir,fo,TrainFileNames[i]))
+            I = I.resize([224, 224])
+            im_array = tf.keras.preprocessing.image.img_to_array(I)
+            targetTensor = np.load(os.path.join(HMtrainDIR,fo,HMTrainFileNames[i]))
+            xTrainData.append(im_array)
+            yTrainData.append(targetTensor)
+            if im_array is None:
+                print("im_array is None")
+    xTrainData=np.array(xTrainData)
+    yTrainData=np.array(yTrainData)
+
     datasetCount = sum([len(files) for r, d, files in os.walk(trainDir)])
     
     log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-    mobileModel.fit(generate_arrays_from_file(HMtrainDIR,trainDir),steps_per_epoch = datasetCount,epochs=1000, 
+    # mobileModel.fit(generate_arrays_from_file(HMtrainDIR,trainDir),steps_per_epoch = datasetCount,epochs=1000, 
+    mobileModel.fit(x=xTrainData,y=yTrainData,steps_per_epoch = datasetCount,epochs=1000,
     validation_data=(xValidationData,yValidationData),callbacks=[tensorboard_callback,reduce_lr,esCallback,checkpoint])
