7393f258d66e6351a53cad97a85b68d128e34a88
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-01-29 11:00:32 -0800

    Update train.py

--- Korcan/train.py
+++ Korcan/train.py
@@ -113,7 +113,7 @@ if __name__ == "__main__":
     mobileModel = loadModel(modelName, splitLayer)
     #mobileModel = tf.keras.models.load_model("checkpoints1/model.44-0.00.h5")
     mobileModel.trainable = True
-    mobileModel.compile(optimizer=tf.keras.optimizers.Adam(1e-3),
+    mobileModel.compile(optimizer=tf.keras.optimizers.Adam(10),
                 loss=tf.keras.losses.MeanAbsoluteError(),)
 
     reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1,patience=4, min_lr=1e-7,min_delta=1e-4,verbose=1)
