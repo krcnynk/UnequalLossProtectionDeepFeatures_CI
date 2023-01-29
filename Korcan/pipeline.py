70810a21947a8f21437ce522e5d771fc7673b47e
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-01-29 11:20:58 -0800

    Update pipeline.py

--- Korcan/pipeline.py
+++ Korcan/pipeline.py
@@ -705,7 +705,7 @@ if __name__ == "__main__":
     cloud_model_path = (
         "deep_models_split/" + modelName + "_" + splitLayer + "_cloud_model.h5"
     )
-    trained_model_path = "/localhome/kuyanik/UnequalLossProtectionDeepFeatures_CI/checkpoints/model.27-0.00.h5"
+    trained_model_path = "/localhome/kuyanik/UnequalLossProtectionDeepFeatures_CI/checkpoints/model.01-2887.05.h5"
     dataName = "/localhome/kuyanik/dataset/largeTest"
     quantizationBits = 8
 
