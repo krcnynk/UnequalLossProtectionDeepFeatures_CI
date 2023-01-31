cac2b98dd777429a689323e7f29ab6268e9d5ed9
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-01-31 02:12:08 -0800

    Update pipeline.py

--- Korcan/pipeline.py
+++ Korcan/pipeline.py
@@ -690,7 +690,7 @@ class pipeline:
         pdictKey = ("{:.3f}".format(100 * packetsLost / packetsSent),case)
         pdictVal = {"acc": metrics["acc"], "loss": metrics["loss"]}
 
-        rand = int(random.random()*1e9)
+        rand = int(random.seed()*1e9)
         with open("Korcan/Plots/"+modelName+"/"+case+"/key_"+str(rand)+".pkl", 'wb') as f:
             pickle.dump(pdictKey, f)
         with open("Korcan/Plots/"+modelName+"/"+case+"/val_"+str(rand)+".pkl", 'wb') as f:
