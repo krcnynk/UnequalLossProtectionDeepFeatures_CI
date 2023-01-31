21756d5d412a85af1e7fee01b704d7b615f9276a
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-01-31 04:04:19 -0800

    Update pipeline.py

--- Korcan/pipeline.py
+++ Korcan/pipeline.py
@@ -767,7 +767,7 @@ if __name__ == "__main__":
                     if f[:3] == "key":
                         with open("Korcan/Plots/"+modelName+"/"+dirs[0]+"/"+f, 'rb') as f:
                             key = pickle.load(f)
-                    elif f[:3] == "val":
+                    if f[:3] == "val":
                         with open("Korcan/Plots/"+modelName+"/"+dirs[0]+"/"+f, 'rb') as f:
                             val = pickle.load(f)
                     self.pdict[key] = val
