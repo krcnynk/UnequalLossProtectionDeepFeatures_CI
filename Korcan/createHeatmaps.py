445bc0c5d5d2bfaa18e20d6505b2fd46b7d4b37f
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-02-01 11:44:34 -0800

    Update createHeatmaps.py

--- Korcan/createHeatmaps.py
+++ Korcan/createHeatmaps.py
@@ -47,7 +47,7 @@ def processDirectory(trainDir,name,HMtrainDir,listOfFilenameLabel,modelPath,grad
         I = tf.keras.preprocessing.image.load_img(os.path.join(trainDir,name,fname))
         I = I.resize([224, 224])
         im_array = tf.keras.preprocessing.image.img_to_array(I)
-        # im_array = tf.keras.applications.densenet.preprocess_input(im_array)
+        im_array = tf.keras.applications.densenet.preprocess_input(im_array)
         im_array = tf.keras.applications.efficientnet.preprocess_input(im_array)
         _ , heatmapTensor = __make_gradcam_heatmap(
             np.expand_dims(im_array, axis=0),
@@ -99,7 +99,7 @@ def findHeatmaps(gradientRespectToLayer,modelName,argumentName,typeProcess):
         I = tf.keras.preprocessing.image.load_img(os.path.join(valDir,argumentName+ ".JPEG"))
         I = I.resize([224, 224])
         im_array = tf.keras.preprocessing.image.img_to_array(I)
-        # im_array = tf.keras.applications.densenet.preprocess_input(im_array)
+        im_array = tf.keras.applications.densenet.preprocess_input(im_array)
         im_array = tf.keras.applications.efficientnet.preprocess_input(im_array)
         _ , heatmapTensor = __make_gradcam_heatmap(
             np.expand_dims(im_array, axis=0),
@@ -111,10 +111,10 @@ def findHeatmaps(gradientRespectToLayer,modelName,argumentName,typeProcess):
             np.save(fil, heatmapTensor)
 
 if __name__ == "__main__":
-    modelName = "efficientnetb0"
-    splitLayer = "block2b_add"
-    # modelName = "dense"
-    # splitLayer = "pool2_conv"
+    # modelName = "efficientnetb0"
+    # splitLayer = "block2b_add"
+    modelName = "dense"
+    splitLayer = "pool2_conv"
     argumentName = sys.argv[1]
     typeProcess = sys.argv[2]
     findHeatmaps(splitLayer,modelName,str(argumentName),int(typeProcess))
