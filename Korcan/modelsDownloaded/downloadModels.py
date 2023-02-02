c088b69bad6138ef3e34d24c433599fe35b728eb
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-02-02 13:59:30 -0800

    Update downloadModels.py

--- Korcan/modelsDownloaded/downloadModels.py
+++ Korcan/modelsDownloaded/downloadModels.py
@@ -8,5 +8,5 @@ tf.keras.models.save_model(model,'../../deep_models_full/efficientnetb0_model.h5
 # model = tf.keras.applications.DenseNet121(weights='imagenet')
 # tf.keras.models.save_model(model,'../../deep_models_full/dense_model.h5',save_format='h5')
 
-model = tf.keras.applications.Resnet50(weights='imagenet')
+model = tf.keras.applications.resnet50.ResNet50(weights='imagenet')
 tf.keras.models.save_model(model,'../../deep_models_full/resnet_model.h5',save_format='h5')
