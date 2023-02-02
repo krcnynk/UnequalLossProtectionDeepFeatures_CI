d9e586723717b0f2eb4fc6c1aaf0563d60707773
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-02-02 13:53:33 -0800

    Update downloadModels.py

--- Korcan/modelsDownloaded/downloadModels.py
+++ Korcan/modelsDownloaded/downloadModels.py
@@ -5,5 +5,8 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 model = tf.keras.applications.EfficientNetB0(weights='imagenet')
 tf.keras.models.save_model(model,'../../deep_models_full/efficientnetb0_model.h5',save_format='h5')
 
-model = tf.keras.applications.DenseNet121(weights='imagenet')
-tf.keras.models.save_model(model,'../../deep_models_full/dense_model.h5',save_format='h5')
+# model = tf.keras.applications.DenseNet121(weights='imagenet')
+# tf.keras.models.save_model(model,'../../deep_models_full/dense_model.h5',save_format='h5')
+
+model = tf.keras.applications.Resnet50(weights='imagenet')
+tf.keras.models.save_model(model,'../../deep_models_full/resnet_model.h5',save_format='h5')
