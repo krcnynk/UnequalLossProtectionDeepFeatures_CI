a0e0f22a0f5f7568e94268a89d649b22d1078b39
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-02-05 00:22:43 -0800

    computeCanadaScripts

---
+++ CCscripts/pipeline.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#module restore uneq
+#source /project/6008756/foniks/Project_1/uneqENV/bin/activate
+rm -r /project/6008756/foniks/Project_1/UnequalLossProtectionDeepFeatures_CI/Korcan/Plots
+source pipelineT.sh
+source pipelineB.sh
+for i in {1..11}
+do
+    source pipelineR.sh
+    source pipelineRRS.sh
+    source pipelineRRSF.sh
+done
+
+#python Korcan/pipeline.py
---
+++ CCscripts/pipelineT.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#!/bin/bash
+#SBATCH --time=4:00:00
+#SBATCH --array=1-11
+#SBATCH --mem=50G
+#SBATCH --account=def-ibajic
+
+module restore uneq
+source /project/6008756/foniks/Project_1/uneqENV/bin/activate
+mkdir -p /project/6008756/foniks/Project_1/UnequalLossProtectionDeepFeatures_CI/Korcan/Plots/resnet/Top
+cd ..
+
+python Korcan/pipeline.py $(($(($i-1))*10)) Top 0 0
