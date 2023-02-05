ef2632201c3a5aed9d15d6c7f12f4e839920ebae
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-02-05 03:24:36 -0800

    Create pipelineGraph.sh

---
+++ CCscripts/pipelineGraph.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --time=2:00:00
+#SBATCH --mem=50G
+#SBATCH --account=def-ibajic
+
+module restore uneq
+source /project/6008756/foniks/Project_1/uneqENV/bin/activate
+cd ..
+python Korcan/pipeline.py 0 makeplot 0 0
