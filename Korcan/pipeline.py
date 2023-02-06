aeb48f454917c99b497c37d9763e19a03f4ed52d
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-02-05 22:03:47 -0800

    Update pipeline.py

--- Korcan/pipeline.py
+++ Korcan/pipeline.py
@@ -92,7 +92,7 @@ class pipeline:
                 colors[i][3] = r[i]
             newcmp = matplotlib.colors.ListedColormap(colors)
             plt.imshow(matrixFeature, interpolation="bilinear", cmap="gray")
-            # matrixHeat = matrixHeat * 3
+            matrixHeat = matrixHeat * 3 #HERE COMMENT
             matrixHeat[matrixHeat > 1] = 1
             plt.imshow(matrixHeat, interpolation="bilinear", cmap=newcmp)
             plt.colorbar()
@@ -265,15 +265,15 @@ class pipeline:
         self.heatMapsChannelsBatch = []
 
         for i_b in range(len(self.dataset_x_files)):
-            # a, b = self.__make_gradcam_heatmap(
-            #     np.expand_dims(np.array(self.dataset_x_files)[i_b], axis=0),
-            #     self.loaded_model,
-            #     gradientRespectToLayer,
-            #     np.array(self.dataset_y_labels_int)[i_b],
-            # )
-            a, b = self.__make_gradcam_heatmap_fromTrainedModel(
+            a, b = self.__make_gradcam_heatmap(
                 np.expand_dims(np.array(self.dataset_x_files)[i_b], axis=0),
+                self.loaded_model,
+                gradientRespectToLayer,
+                np.array(self.dataset_y_labels_int)[i_b],
             )
+            # a, b = self.__make_gradcam_heatmap_fromTrainedModel(
+            #     np.expand_dims(np.array(self.dataset_x_files)[i_b], axis=0),
+            # )
             self.heatmapsBatch.append(a)
             self.heatMapsChannelsBatch.append(b)
         self.heatmapsBatch = np.array(self.heatmapsBatch)
