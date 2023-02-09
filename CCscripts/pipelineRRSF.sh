ca2645dfddf6138435f85e24378dfd6f84a23842
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-02-08 17:55:29 -0800

    cc

--- CCscripts/pipelineRRSF.sh
+++ CCscripts/pipelineRRSF.sh
@@ -6,7 +6,7 @@
 
 module restore uneq
 source /project/6008756/foniks/Project_1/uneqENV/bin/activate
-mkdir -p /project/6008756/foniks/Project_1/UnequalLossProtectionDeepFeatures_CI/Korcan/Plots/resnet/Random_RSCorrected_FECRemovesBOT_40_60
+mkdir -p /project/6008756/foniks/Project_1/UnequalLossProtectionDeepFeatures_CI/Korcan/Plots/resnet/Random_RSCorrected_FECRemovesBOT_20_80
 cd ..
 
-python Korcan/pipeline.py $(($(($SLURM_ARRAY_TASK_ID-1))*10)) Random_RSCorrected_FECRemovesBOT 40 60
+python Korcan/pipeline.py $(($(($SLURM_ARRAY_TASK_ID-1))*10)) Random_RSCorrected_FECRemovesBOT 20 80
