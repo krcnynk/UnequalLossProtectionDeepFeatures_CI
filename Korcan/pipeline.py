c973ccf642d0aa5aefc2399d2d40a0645dd3d1f5
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-02-09 05:19:12 -0800

    Update pipeline.py

--- Korcan/pipeline.py
+++ Korcan/pipeline.py
@@ -321,11 +321,11 @@ class pipeline:
         cases = ["Top","b","1","-",
         "Bot","g","+","-",
         "Random","r",".","-",
-        "Random_RSCorrected_FECRemovesBOT_10_90","m","h","-",
-        "Random_RSCorrected_FECRemovesBOT_20_80","m","d","--",
-        "Random_RSCorrected_FECRemovesBOT_30_70","m","*","-.",
-        "Random_RSCorrected_FECRemovesBOT_40_60","m","s","solid",
-        "Random_RSCorrected_FECRemovesBOT_50_50","m","p",":",]
+        "R_RS_FEC_10_90","m","h","-",
+        "R_RS_FEC_20_80","m","d","--",
+        "R_RS_FEC_30_70","m","*","-.",
+        "R_RS_FEC_40_60","m","s","solid",
+        "R_RS_FEC_50_50","m","p",":",]
         types = list(set([i[1] for i in self.pdict.keys()]))
         seriesX = [[] for _ in range(len(types))]
         seriesY = [[] for _ in range(len(types))]
