fe428b12ced282ad9a75a0afc63b4fdb6ea0b54a
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-02-09 19:11:13 -0800

    Update pipeline.py

--- Korcan/pipeline.py
+++ Korcan/pipeline.py
@@ -353,11 +353,11 @@ class pipeline:
         
         # specify order
         print("HEY",len(handles))
-        order=[0,6,5,1,2,3,4]
+        # order=[0,6,5,1,2,3,4]
         # order=[3,5,6, 2,0,1,7,4]
 
         plt.legend(
-            [handles[i] for i in order], [labels[i] for i in order],
+            # [handles[i] for i in order], [labels[i] for i in order],
             loc="upper right",
             fontsize="xx-small",
             markerscale=0.7,
@@ -834,7 +834,7 @@ if __name__ == "__main__":
                         dp = "Least imporant"
                         key[1] = dp
                         key = tuple(key)
-                        
+
                     module.pdict[key] = val
         dirNames = []
         for fp in fpPairs:
