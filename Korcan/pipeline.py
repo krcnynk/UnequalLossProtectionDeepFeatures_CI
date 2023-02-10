4bdc26043e9cdc4780ecf7e6df2204e96d5d185a
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-02-09 19:19:02 -0800

    Update pipeline.py

--- Korcan/pipeline.py
+++ Korcan/pipeline.py
@@ -340,9 +340,9 @@ class pipeline:
         for s in range(len(seriesX)):
             mapping = cases.index(types[s])
             seriesX[s], seriesY[s] = zip(*sorted(zip(seriesX[s], seriesY[s])))
-            if(types[s]=="Top" or types[s]=="Bot" or types[s]=="Random"):
-                plt.scatter(seriesX[s], seriesY[s],s=20, label=cases[mapping], marker=cases[mapping+2],color=cases[mapping+1])
-                plt.plot(seriesX[s], seriesY[s],label='_nolegend_',linestyle=cases[mapping+3] ,linewidth=1.2, color=cases[mapping+1])
+            if(types[s]=="Least imporant" or types[s]=="Most important" or types[s]=="Random"):
+                plt.scatter(seriesX[s], seriesY[s],s=20, label='_nolegend_', marker=cases[mapping+2],color=cases[mapping+1])
+                plt.plot(seriesX[s], seriesY[s],label=cases[mapping],linestyle=cases[mapping+3] ,linewidth=1.2, color=cases[mapping+1])
             else:
                 print(types[s])
                 plt.scatter(seriesX[s], seriesY[s],s=20, marker=cases[mapping+2],color=cases[mapping+1])
