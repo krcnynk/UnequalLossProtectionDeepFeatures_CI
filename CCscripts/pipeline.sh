f72c170ec61832535d958da95f2775126dfc485d
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-02-12 00:33:27 -0800

    cc

--- CCscripts/pipeline.sh
+++ CCscripts/pipeline.sh
@@ -6,11 +6,9 @@ git pull
 rm -r /project/6008756/foniks/Project_1/UnequalLossProtectionDeepFeatures_CI/Korcan/Plots
 sbatch pipelineT.sh
 sbatch pipelineB.sh
-for i in {1..10}
-do
-    sbatch pipelineR.sh
-    sbatch pipelineRRS.sh
-    sbatch pipelineRRSF.sh
-done
-
-#python Korcan/pipeline.py
+# for i in {1..10}
+# do
+#     sbatch pipelineR.sh
+#     sbatch pipelineRRS.sh
+#     sbatch pipelineRRSF.sh
+# done
