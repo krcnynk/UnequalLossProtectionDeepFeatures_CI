fb6408ac5cfd1d968704af430ea2ec5f8fa17de6
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-02-13 17:30:14 -0800

    Update pipeline.py

--- Korcan/pipeline.py
+++ Korcan/pipeline.py
@@ -781,7 +781,7 @@ if __name__ == "__main__":
     case = sys.argv[2]
 
     module.saveSuperImposedChannels(modelName)
-    saveImageLossPercent = 50
+    saveImageLossPercent = 40
     module.packetLossSim(
         packetCount, quantizationBits, saveImageLossPercent, "Top", saveImages=True,modelName=modelName
     )
