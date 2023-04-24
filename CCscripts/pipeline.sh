9aab8d82aef1723fc8586e447b376607e1c995e7
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-04-24 11:22:16 -0700

    Update pipeline.sh

--- CCscripts/pipeline.sh
+++ CCscripts/pipeline.sh
@@ -3,13 +3,13 @@
 #module restore uneq
 #source /project/6008756/foniks/Project_1/uneqENV/bin/activate
 git pull
-rm -r /project/6008756/foniks/Project_1/UnequalLossProtectionDeepFeatures_CI/Korcan/Plots
+# rm -r /project/6008756/foniks/Project_1/UnequalLossProtectionDeepFeatures_CI/Korcan/Plots
 sbatch pipelineT.sh
 sbatch pipelineB.sh
-for i in {1..10}
-do
-    sbatch pipelineRBUR.sh
-    sbatch pipelineRIID.sh
-    sbatch pipelineRSBUR.sh
-    sbatch pipelineRSIID.sh
-done
+# for i in {1..10}
+# do
+#     sbatch pipelineRBUR.sh
+#     sbatch pipelineRIID.sh
+#     sbatch pipelineRSBUR.sh
+#     sbatch pipelineRSIID.sh
+# done
