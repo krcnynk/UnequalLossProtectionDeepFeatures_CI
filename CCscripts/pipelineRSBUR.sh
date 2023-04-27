d7b60c5e6bcf306c22ab119c8689607fb7943a81
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-04-27 02:06:29 -0700

    y

--- CCscripts/pipelineRSBUR.sh
+++ CCscripts/pipelineRSBUR.sh
@@ -9,4 +9,4 @@ module restore uneq
 # mkdir -p /project/6008756/foniks/Project_1/UnequalLossProtectionDeepFeatures_CI/Korcan/Plots/resnet/Random_RSCorrected_20_80
 cd ..
 
-python Korcan/pipeline.py $(($(($SLURM_ARRAY_TASK_ID-1))*10)) 6 50 50
+python Korcan/pipeline.py $(($(($SLURM_ARRAY_TASK_ID-1))*10)) 6 40 60
