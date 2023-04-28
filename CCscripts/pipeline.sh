b00db9ba58e5536b7b0b0e0210fd728b433e2536
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-04-28 14:17:44 -0700

    y

--- CCscripts/pipeline.sh
+++ CCscripts/pipeline.sh
@@ -5,15 +5,22 @@
 git pull
 rm -r /project/6008756/foniks/Project_1/UnequalLossProtectionDeepFeatures_CI/Korcan/Plots
 
-sbatch pipelineT.sh
-sbatch pipelineB.sh
-for i in {1..10}
+# sbatch pipelineT.sh
+# sbatch pipelineB.sh
+for i in {1..2}
 do
-    sbatch pipelineRBUR.sh
-    sbatch pipelineRIID.sh
-    sbatch pipelineRSBUR.sh
-    sbatch pipelineRSIID.sh
-    sbatch pipelineRBURNS.sh
-    sbatch pipelineRIIDNS.sh
-    sbatch pipelineRIIDEN.sh
+    # sbatch pipelineRBUR.sh
+    # sbatch pipelineRIID.sh
+
+
+    # sbatch pipelineRBURNS.sh
+    # sbatch pipelineRIIDNS.sh
+    # sbatch pipelineRIIDEN.sh
+
+
+    # sbatch pipelineRSBUR.sh
+    # sbatch pipelineRSIID.sh
+
+    sbatch pipelineRSBURNS.sh
+    sbatch pipelineRSIIDNS.sh
 done
---
+++ CCscripts/pipelineRSBURNS.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=4:00:00
+#SBATCH --array=1-11
+#SBATCH --mem=50G
+#SBATCH --account=def-ibajic
+
+module restore uneq
+# source /project/6008756/foniks/Project_1/uneqENV/bin/activate
+# mkdir -p /project/6008756/foniks/Project_1/UnequalLossProtectionDeepFeatures_CI/Korcan/Plots/resnet/Random_RSCorrected_20_80
+cd ..
+
+python Korcan/pipeline.py $(($(($SLURM_ARRAY_TASK_ID-1))*10)) 6 40 60
