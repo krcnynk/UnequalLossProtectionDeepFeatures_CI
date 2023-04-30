52ee6293d727dab6adfc8f01acb932d3524a3a2d
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-04-30 04:07:24 -0700

    Update pipeline.sh

--- CCscripts/pipeline.sh
+++ CCscripts/pipeline.sh
@@ -5,21 +5,21 @@
 git pull
 rm -r /project/6008756/foniks/Project_1/UnequalLossProtectionDeepFeatures_CI/Korcan/Plots
 
-# sbatch pipelineT.sh
-# sbatch pipelineB.sh
+sbatch pipelineT.sh
+sbatch pipelineB.sh
 for i in {1..2}
 do
-    # sbatch pipelineRBUR.sh
-    # sbatch pipelineRIID.sh
+    sbatch pipelineRBUR.sh
+    sbatch pipelineRIID.sh
 
 
-    # sbatch pipelineRBURNS.sh
-    # sbatch pipelineRIIDNS.sh
-    # sbatch pipelineRIIDEN.sh
+    sbatch pipelineRBURNS.sh
+    sbatch pipelineRIIDNS.sh
+    sbatch pipelineRIIDEN.sh
 
 
-    # sbatch pipelineRSBUR.sh
-    # sbatch pipelineRSIID.sh
+    sbatch pipelineRSBUR.sh
+    sbatch pipelineRSIID.sh
 
     sbatch pipelineRSBURNS.sh
     sbatch pipelineRSIIDNS.sh
