4b4a1dde20a1ce13129868d1710054c7dc4dc358
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-05-08 04:33:09 -0700

    Update pipelineRIIDEN.sh

--- CCscripts/pipelineRIIDEN.sh
+++ CCscripts/pipelineRIIDEN.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#SBATCH --time=1:00:00
-#SBATCH --mem=1G
+#SBATCH --time=4:00:00
+#SBATCH --array=1-11
+#SBATCH --mem=50G
 #SBATCH --account=def-ibajic
 
 module restore uneq
 # source /project/6008756/foniks/Project_1/uneqENV/bin/activate
 # mkdir -p /project/6008756/foniks/Project_1/UnequalLossProtectionDeepFeatures_CI/Korcan/Plots/resnet/Random
 cd ..
-echo "Argument passed to script: $arg1"
-# python Korcan/pipeline.py $(($(($SLURM_ARRAY_TASK_ID-1))*10)) 9 0 $arg1
+
+python Korcan/pipeline.py $(($(($SLURM_ARRAY_TASK_ID-1))*10)) 9 0 $arg1
