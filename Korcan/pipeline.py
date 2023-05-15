89fdc8fd01e34cb2e4c727860fe82d0c53847318
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-05-15 15:09:04 -0700

    Update pipeline.py

--- Korcan/pipeline.py
+++ Korcan/pipeline.py
@@ -882,9 +882,9 @@ class pipeline:
 
                 tensorEncodedBufferSize = 0
                 encode_param = [int(cv.IMWRITE_JPEG_QUALITY), qualityFactor]
-                if qualityFactor == 105:
-                    OrderedImportanceOfPacketsIndexExcludeFEC =
-                    encode_param = [i for i in range(101)]
+                # if qualityFactor == 105:
+                #     OrderedImportanceOfPacketsIndexExcludeFEC =
+                #     encode_param = [i for i in range(101)]
                 
                 for j in range(len(packetizedfmL)):
                     # print(len(packetizedfmL))
