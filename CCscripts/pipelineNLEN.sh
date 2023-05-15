0a6e8b574a849530e2a131e3c74486b1a1dd1bfd
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-05-15 13:37:59 -0700

    Update pipelineNLEN.sh

--- CCscripts/pipelineNLEN.sh
+++ CCscripts/pipelineNLEN.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #SBATCH --time=4:00:00
+#SBATCH --array=1-11
 #SBATCH --mem=50G
 #SBATCH --account=def-ibajic
 
@@ -8,4 +9,4 @@ module restore uneq
 # mkdir -p /project/6008756/foniks/Project_1/UnequalLossProtectionDeepFeatures_CI/Korcan/Plots/resnet/Random
 cd ..
 
-python Korcan/pipeline.py 0 9 0 $arg1
+python Korcan/pipeline.py 0 9 0 $(($(($SLURM_ARRAY_TASK_ID-1))*10))
