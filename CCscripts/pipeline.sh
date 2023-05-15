61c76460f4c7ba402b9ba67190e98c2f4b2f1c94
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-05-15 01:50:39 -0700

    Update pipeline.sh

--- CCscripts/pipeline.sh
+++ CCscripts/pipeline.sh
@@ -13,14 +13,16 @@ mkdir -p /project/6008756/foniks/Project_1/UnequalLossProtectionDeepFeatures_CI/
 #     # echo "$i"
 #     sbatch --export=arg1=$i pipelineRIIDEN.sh
 # done
+for i in {0..100..10}
+do
+    sbatch --export=arg1=$i pipelineRIIDEN.sh
+done
 
 for i in {1..1}
 do
     # sbatch pipelineRBUR.sh
     # sbatch pipelineRIID.sh
 
-
-    sbatch --export=arg1=80 pipelineRIIDEN.sh
     # sbatch --export=arg1=80 pipelineRIIDEN.sh
 
     # sbatch pipelineRSBUR.sh
