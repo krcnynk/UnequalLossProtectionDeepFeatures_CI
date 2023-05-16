0bcac846293252d179815922a24cd78493a1b498
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-05-16 04:02:45 -0700

    Update pipeline.py

--- Korcan/pipeline.py
+++ Korcan/pipeline.py
@@ -415,8 +415,8 @@ class pipeline:
             _, seriesYmax[s] = zip(*sorted(zip(seriesX[s], seriesYmax[s])))
             _, seriesYmin[s] = zip(*sorted(zip(seriesX[s], seriesYmin[s])))
             seriesX[s], seriesY[s] = zip(*sorted(zip(seriesX[s], seriesY[s])))
-            print(l[-1:])
-            if types[s] is l[-4:]:
+            print(l[-4:])
+            if types[s] == l[-4:]:
 
                 plt.axhline(y=seriesY[s], color='red', linestyle='--')
             # if (
