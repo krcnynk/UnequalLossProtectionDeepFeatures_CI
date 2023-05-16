f0eaa518c4be1f590cf72b512acfd816736e30a3
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-05-16 04:14:50 -0700

    Update pipeline.py

--- Korcan/pipeline.py
+++ Korcan/pipeline.py
@@ -417,7 +417,7 @@ class pipeline:
             seriesX[s], seriesY[s] = zip(*sorted(zip(seriesX[s], seriesY[s])))
             if types[s] in l[-4:]:
                 print(l[-4:])
-                l[-3:-2] = "r"
+                cases[-3:-2] = "r"
                 print(l[-4:])
                 plt.axhline(y=seriesY[s], color='red', linestyle='--')
             # if (
