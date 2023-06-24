8103459fd17658225bf8bea287bee4de3e8e4cfc
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-06-24 14:47:23 -0700

    Update pipeline.py

--- Korcan/pipeline.py
+++ Korcan/pipeline.py
@@ -101,7 +101,7 @@ class pipeline:
             newcmp = matplotlib.colors.ListedColormap(colors)
             plt.imshow(matrixFeature, interpolation="bilinear", cmap="gray")
             matrixHeat = (matrixHeat - np.min(matrixHeat)) / (np.max(matrixHeat) - np.min(matrixHeat))
-            matrixHeat = matrixHeat * 5 # HERE COMMENT
+            matrixHeat = matrixHeat * 2 # HERE COMMENT
             matrixHeat[matrixHeat > 1] = 1
             # matrixHeat[matrixHeat < 0.8] = 0.6
             matrixHeat[matrixHeat < 0.5] = 0.2
