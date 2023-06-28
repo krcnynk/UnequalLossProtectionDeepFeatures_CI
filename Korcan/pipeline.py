47fdd3ae1c2cf92b82cc16cdc642afa81a53dae8
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-06-28 16:07:00 -0700

    Update pipeline.py

--- Korcan/pipeline.py
+++ Korcan/pipeline.py
@@ -810,7 +810,7 @@ class pipeline:
                     # print(f'Packet {i_pkt} in channel {i_c} repaired.')
 
         for i_c in lostChannels:
-            mse_values = [np.mean((tensorCompletedNoLoss[:,:,i_c] - pkt_obj[li_c, :, :])**2) for li_c in lostChannels]
+            mse_values = [np.mean((pktzNoLoss[i_c,:,:] - pkt_obj[li_c, :, :])**2) for li_c in lostChannels]
             # Find the index of the most similar matrix based on the lowest MSE value
             most_similar_index = np.argmin(mse_values)
             pkt_obj[i_c, :, :] = pkt_obj[most_similar_index, :, :]
