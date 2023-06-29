c2af8e7a6653e9694df9ad7c8cb5fe895a3cc30b
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-06-28 20:38:45 -0700

    Update pipeline.py

--- Korcan/pipeline.py
+++ Korcan/pipeline.py
@@ -815,8 +815,8 @@ class pipeline:
             mse_values = [np.mean((pktzNoLoss[i_c,:,:] - pkt_obj[li_c, :, :])**2) for li_c in lostChannels]
             # Find the index of the most similar matrix based on the lowest MSE value
             most_similar_index = np.argmin(mse_values)
-            # print(mse_values)
-            pkt_obj[i_c, :, :] = pkt_obj[4, :, :]
+            print(i_c,most_similar_index)
+            pkt_obj[i_c, :, :] = pkt_obj[most_similar_index, :, :]
         #     corrcoeff_matrix = np.corrcoef(
         #     [
         #         np.reshape(
