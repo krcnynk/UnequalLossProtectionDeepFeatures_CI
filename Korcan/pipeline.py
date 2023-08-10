70397bb5b250f2bb681bcbde8eac4fd492a96ad1
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-08-09 23:37:36 -0700

    Update pipeline.py

--- Korcan/pipeline.py
+++ Korcan/pipeline.py
@@ -1875,10 +1875,10 @@ if __name__ == "__main__":
         #         fpPairs.append(splitted[3:])  # FEC_10_50
 
         dirNames = []
-        # dirNames.append("Most important")
-        # dirNames.append("Most important NS")
-        # dirNames.append("Least important")
-        # dirNames.append("Least important NS")
+        dirNames.append("Most important")
+        dirNames.append("Most important NS")
+        dirNames.append("Least important")
+        dirNames.append("Least important NS")
 
         # dirNames.append("Most important Weighted")
         # dirNames.append("Most important NS Weighted")
@@ -1909,9 +1909,9 @@ if __name__ == "__main__":
         # dirNames.append("FEC (Burst)")
         # dirNames.append("FEC (Burst) NS")
 
-        # dirNames.append("Unprotected (IID)")
+        dirNames.append("Unprotected (IID)")
+        dirNames.append("Unprotected (IID) NS")
         # dirNames.append("Unprotected (Burst)")
-        # dirNames.append("Unprotected (IID) NS")
         # dirNames.append("Unprotected (Burst) NS")
         # dirNames = []
 
