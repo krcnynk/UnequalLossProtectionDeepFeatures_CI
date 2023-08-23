1cc920abe5805fc21178c71e710fedc71e4d4954
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-08-23 13:28:30 -0700

    y

--- Korcan/pipeline.py
+++ Korcan/pipeline.py
@@ -1902,7 +1902,7 @@ if __name__ == "__main__":
         dirNames = []
 
         dirNames.append("FEC (IID) Weighted")
-        dirNames.append("FEC (IID) NS Weighted")
+        # dirNames.append("FEC (IID) NS Weighted")
         dirNames.append("FEC (IID)")
         dirNames.append("FEC (IID) NS")
         # dirNames.append("FEC (Burst)")
