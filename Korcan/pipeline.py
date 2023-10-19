450cc77d1ec03e93b909c8a3ee565e18852c6a0f
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-10-19 14:08:22 -0700

    Update pipeline.py

--- Korcan/pipeline.py
+++ Korcan/pipeline.py
@@ -1055,11 +1055,28 @@ class pipeline:
                 # 
                 # 
 ############################################# ##########  KorcanRandom
+                # NoImportanceIndex = OrderedImportanceOfPacketsIndex[math.floor(len(importanceOfPackets)*60/100):]
+                # TopImportanteIndex = OrderedImportanceOfPacketsIndex[:math.floor(len(importanceOfPackets)*20/100)]
+                # maxX = max(importanceOfPackets[NoImportanceIndex])
+                # selected_ind = [i for i in random.sample(NoImportanceIndex,math.floor(len(importanceOfPackets)*20/100))]
+                # importanceOfPacketsWeighted[selected_ind] = maxX
+
+############################################# ##########  KorcanRandom2
                 NoImportanceIndex = OrderedImportanceOfPacketsIndex[math.floor(len(importanceOfPackets)*60/100):]
-                TopImportanteIndex = OrderedImportanceOfPacketsIndex[:math.floor(len(importanceOfPackets)*20/100)]
-                maxX = max(importanceOfPackets[NoImportanceIndex])
-                selected_ind = [i for i in random.sample(NoImportanceIndex,math.floor(len(importanceOfPackets)*20/100))]
-                importanceOfPacketsWeighted[selected_ind] = maxX
+                bin_size = 8
+                # Initialize a dictionary to store the bins
+                bins = {}
+                # Iterate through the random indices and assign them to bins
+                for index in NoImportanceIndex:
+                    bin_number = index // bin_size
+                    if bin_number not in bins:
+                        bins[bin_number] = []
+                    bins[bin_number].append(index)
+
+                # Convert the bins dictionary to a list
+                bins_list = [indices for _, indices in sorted(bins.items())]
+                sorted_bins = sorted(bins_list, key=len, reverse=True)
+                combined_list = [index for indices in sorted_bins for index in indices]
 ############################################# ##########  Korcan2
 
                 # NoImportanceIndex = OrderedImportanceOfPacketsIndex[math.floor(len(importanceOfPackets)*60/100):]
