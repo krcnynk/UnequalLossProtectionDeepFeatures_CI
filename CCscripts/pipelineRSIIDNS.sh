7fc941931d89062ad22d7c783482b80764d94e83
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-10-22 23:51:04 -0700

    y

--- CCscripts/pipelineRSIIDNS.sh
+++ CCscripts/pipelineRSIIDNS.sh
@@ -9,7 +9,7 @@ module restore uneq
 # mkdir -p /project/6008756/foniks/Project_1/UnequalLossProtectionDeepFeatures_CI/Korcan/Plots/resnet/Random_RSCorrected_20_80
 cd ..
 
-python Korcan/pipeline.py $(($(($SLURM_ARRAY_TASK_ID-1))+30)) 11 30 70
-python Korcan/pipeline.py $(($(($SLURM_ARRAY_TASK_ID-1))*10)) 11 30 70
+python Korcan/pipeline.py $(($(($SLURM_ARRAY_TASK_ID-1))+50)) 11 50 50
+python Korcan/pipeline.py $(($(($SLURM_ARRAY_TASK_ID-1))*10)) 11 50 50
 # python Korcan/pipeline.py $(($(($SLURM_ARRAY_TASK_ID-1))*10)) 20 40 60
 # python Korcan/pipeline.py $(($(($SLURM_ARRAY_TASK_ID-1))+40)) 20 40 60
--- CCscripts/pipelineRSIIDNSW.sh
+++ CCscripts/pipelineRSIIDNSW.sh
@@ -11,5 +11,5 @@ cd ..
 
 # python Korcan/pipeline.py $(($(($SLURM_ARRAY_TASK_ID-1))+40)) 11 40 60
 # python Korcan/pipeline.py $(($(($SLURM_ARRAY_TASK_ID-1))*10)) 11 40 60
-python Korcan/pipeline.py $(($(($SLURM_ARRAY_TASK_ID-1))*10)) 20 30 70
-python Korcan/pipeline.py $(($(($SLURM_ARRAY_TASK_ID-1))+30)) 20 30 70
+python Korcan/pipeline.py $(($(($SLURM_ARRAY_TASK_ID-1))*10)) 20 50 50
+python Korcan/pipeline.py $(($(($SLURM_ARRAY_TASK_ID-1))+50)) 20 50 50
