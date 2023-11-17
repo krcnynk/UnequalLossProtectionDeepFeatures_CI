36f67060664fa892c3ad4bd85de9af746d01d3be
Korcan Uyanik <71324101+krcnynk@users.noreply.github.com>
2023-11-17 01:48:06 -0800

    Update pipeline.py

--- Korcan/pipeline.py
+++ Korcan/pipeline.py
@@ -1080,7 +1080,7 @@ class pipeline:
                 # selected_ind = [i for i in random.sample(NoImportanceIndex,math.floor(len(importanceOfPackets)*30/100))]
                 # importanceOfPacketsWeighted[selected_ind] = maxX
 ############################################ ##########  KorcanRandom2
-                NoImportanceIndex = OrderedImportanceOfPacketsIndex[math.floor(len(importanceOfPackets)*60/100):]
+                NoImportanceIndex = OrderedImportanceOfPacketsIndex[math.floor(len(importanceOfPackets)*50/100):]
                 maxX = min(importanceOfPackets[NoImportanceIndex]) - 1
                 bin_size = 8
                 # Initialize a dictionary to store the bins
@@ -1096,7 +1096,7 @@ class pipeline:
                 bins_list = [indices for _, indices in sorted(bins.items())]
                 sorted_bins = sorted(bins_list, key=len, reverse=True)
                 combined_list = [index for indices in sorted_bins for index in indices]
-                importanceOfPacketsWeighted[combined_list[:math.floor(len(importanceOfPackets)*20/100)]] = maxX
+                importanceOfPacketsWeighted[combined_list[:math.floor(len(importanceOfPackets)*30/100)]] = maxX
 
 ############################################# ##########  KorcanRandom3
                 # NoImportanceIndex = OrderedImportanceOfPacketsIndex[math.floor(len(importanceOfPackets)*20/100):]
